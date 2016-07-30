e3bc2c94b3bc3a9eebf671a68ef2dd3d5be4e72a
Michael Lattanzio <mlattanz25@gmail.com>
2016-07-30 21:55:19 +0000

    First Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+end
+
+
+#might belong in index.html.erb page <p id="notice"><%= notice %></p>
---
+++ app/models/listing.rb
@@ -0,0 +1,11 @@
+class Listing < ActiveRecord::Base
+ 
+    has_attached_file :image, styles: { medium: "400x", thumb: "50x50>" }, default_url: "pool3.jpg"
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/   
+
+end
+
+
+=begin
+
+=end
